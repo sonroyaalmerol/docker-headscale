f12cf503153f4e34fa148498418c8a774bace88c
Son Roy Almerol <sralmerol@sgl.com>
2024-05-09 13:31:59 -0400

    init: commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM debian:bookworm-slim
+
+ARG VERSION=0.22.3
+ARG ARCH=amd64
+
+ADD https://github.com/juanfont/headscale/releases/download/v${VERSION}/headscale_${VERSION}_linux_${ARCH}.deb /tmp/headscale.deb
+
+ADD https://github.com/mikefarah/yq/releases/latest/download/yq_linux_${ARCH} /usr/bin/yq
+
+COPY ./entrypoint.sh /entrypoint.sh
+
+RUN apt-get update \
+  && apt-get install /tmp/headscale.deb \
+  && rm -rf /var/lib/apt/lists/* \
+  && apt-get clean \
+  && rm -rf /tmp/headscale.deb \
+  && chmod +x /usr/bin/yq
+
+EXPOSE 8080/tcp
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+config_file=/etc/headscale/config.yaml
+tmp_config_file=/etc/headscale/config_tmp.yaml
+
+# Scan env variables
+env | {
+template_formatted=""
+while IFS= read -r line; do
+  value=${line#*=}
+  key=${line%%=*}
+  key="${key^}"
+
+  # Convert them to .properties format for yq
+  if [[ "${key}" =~ ^HS_.* ]]; then
+    key="${key//HS_/}"
+    key="${key,,}"
+    key="${key//__/.}"
+    template_formatted="${template_formatted}"$'\n'"${key}=${value}"
+  fi
+done
+
+echo -e "$template_formatted"
+} | yq -p=props -oy - | sed 's/"true"/true/g;s/"false"/false/g' | tee -a "$tmp_config_file"
+
+yq ". *= load(\"$tmp_config_file\")" "$config_file"
+
+/usr/bin/headscale serve
